0ed47ff9dfac626703627389cad74469980d4791
werxic <33441809+werxic@users.noreply.github.com>
2017-11-07 16:07:32 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-// example: https://github.com/onlinetuts/line-bot-api/blob/master/php/example/chapter-01.php
+// example: https://github.com/onlinetuts/line-bot-api/blob/master/php/example/chapter-02.php
 
 include ('line-bot-api/php/line-bot.php');
 
@@ -9,17 +9,13 @@ $access_token  = 'pwIlcuXib+mh3MODhEytMPdEmMODfG2vk5kwkqN5ohp7goT5tpxUIsaMr7BJ9j
 
 $bot = new BOT_API($channelSecret, $access_token);
 	
-if (!empty($bot->isEvents)) {
-		
-	$bot->replyMessageNew($bot->replyToken, json_encode($bot->message));
+$bot->sendMessageNew('[Your userId / User Id]', 'Hello World !!');
 
-	if ($bot->isSuccess()) {
-		echo 'Succeeded!';
-		exit();
-	}
-
-	// Failed
-	echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody(); 
+if ($bot->isSuccess()) {
+	echo 'Succeeded!';
 	exit();
-
 }
+
+// Failed
+echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody(); 
+exit();
