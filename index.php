c501046c4e10def6fb4cfcadfbe396c709852f7b
werxic <33441809+werxic@users.noreply.github.com>
2017-11-07 18:35:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -11,21 +11,21 @@ $arrHeader = array();
 $arrHeader[] = "Content-Type: application/json";
 $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
  
-if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
+if($arrJson['events'][0]['message']['text'] == "mode 1"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$arrJson['events'][0]['source']['userId'];
-}else if($arrJson['events'][0]['message']['text'] == "柰托涿"){
+  $arrPostData['messages'][0]['text'] = "status : อยู่บ้าน";
+}else if($arrJson['events'][0]['message']['text'] == "mode 2"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "┭孤学淞枇摘阻凸�";
-}else if($arrJson['events'][0]['message']['text'] == "酚托涿浯楹橐�"){
+  $arrPostData['messages'][0]['text'] = "status : ไม่อยู่บ้าน";
+}else if($arrJson['events'][0]['message']['text'] == "mode 3"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "┭狗油袖娩凌浯猷怕 へ车橥凸┭雇铡嗦托";
+  $arrPostData['messages'][0]['text'] = "status : พักผ่อน";
 }else{
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
