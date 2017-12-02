c503997fc292923cc48ff4329f9798746b6e4a0d
werxic <33441809+werxic@users.noreply.github.com>
2017-12-02 23:45:04 +0700

    Update pub.php

--- pub.php
+++ pub.php
@@ -8,15 +8,53 @@
   }
   function getMqttfromlineMsg($lineMsg){
  
-    $pos = strpos($lineMsg, ":");
-    if($pos){
-      $splitMsg = explode(":", $lineMsg);
-      $topic = $splitMsg[0];
-      $msg = $splitMsg[1];
+	if ($lineMsg === "mode A"){ 	
+      $topic = "ich_mode";
+      $msg = 1;
       pubMqtt($topic,$msg);
-    }else{
-      $topic = "raw";
-      $msg = $lineMsg;
+    }else if ($lineMsg === "mode B"){ 	
+      $topic = "ich_mode";
+      $msg = 2;
+      pubMqtt($topic,$msg);
+    }else if ($lineMsg === "mode C"){ 	
+      $topic = "ich_mode";
+      $msg = 3;
+      pubMqtt($topic,$msg);
+    }else if ($lineMsg === "mode D"){ 	
+      $topic = "ich_mode";
+      $msg = 4;
+      pubMqtt($topic,$msg);
+    }else if ($lineMsg === "bed 1"){ 	
+      $topic = "ich_mode";
+      $msg = 5;
+      pubMqtt($topic,$msg);
+    }else if ($lineMsg === "bed 0"){ 	
+      $topic = "ich_mode";
+      $msg = 6;
+      pubMqtt($topic,$msg);
+    }else if ($lineMsg === "liv 1"){ 	
+      $topic = "ich_mode";
+      $msg = 7;
+      pubMqtt($topic,$msg);
+    }else if ($lineMsg === "liv 0"){ 	
+      $topic = "ich_mode";
+      $msg = 8;
+      pubMqtt($topic,$msg);
+    }else if ($lineMsg === "kit 1"){ 	
+      $topic = "ich_mode";
+      $msg = 9;
+      pubMqtt($topic,$msg);
+    }else if ($lineMsg === "kit 0"){ 	
+      $topic = "ich_mode";
+      $msg = 10;
+      pubMqtt($topic,$msg);
+    }else if ($lineMsg === "fan 1"){ 	
+      $topic = "ich_mode";
+      $msg = 11;
+      pubMqtt($topic,$msg);
+    }else if ($lineMsg === "fan 0"){ 	
+      $topic = "ich_mode";
+      $msg = 12;
       pubMqtt($topic,$msg);
     }
   }
