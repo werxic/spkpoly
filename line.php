24b6812521fb98001ed21cd34d30b679c2a98959
werxic <33441809+werxic@users.noreply.github.com>
2017-12-02 22:22:29 +0700

    Update line.php

--- line.php
+++ line.php
@@ -7,7 +7,7 @@ require('pub.php');
 
 $channelAccessToken = 'zJzB+JIl4LFRGuSDmxB1rN5SFfoL0pPAi9JLo4VPlcuioZ3Z5Uk7KZq3Rw2IKXfczBMcPLl9ckbGCXdOQiRGFuwQfIGqmzkXVzw8roAa4QfxUhwnh2VIWeIaHWwLx7jaetUR5KKGVzU3+37WlPcgDAdB04t89/1O/w1cDnyilFU=';
 $channelSecret = 'a89901877671b7e3c1030fd266ddaf00';
-
+$mode_D = 0;
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 foreach ($client->parseEvents() as $event) {
     switch ($event['type']) {
@@ -24,6 +24,21 @@ foreach ($client->parseEvents() as $event) {
 					}else if($message['text']==="mode C"){
 						$mixmsg = 'คุณกำลังจะพักผ่อน';
 					}else if($message['text']==="mode D"){
+			    			while(mode_D==1){
+							if($message['text']==="bed 1"){
+								$mixmsg = 'ไฟห้องนอน:เปิด';
+							}else if($message['text']==="bed 0"){
+								$mixmsg = 'ไฟห้องนอน:ปิด';
+							}else if($message['text']==="liv 1"){
+								$mixmsg = 'ไฟห้องนั่งเล่น:เปิด';
+							}else if($message['text']==="liv 0"){
+								$mixmsg = 'ไฟห้องนั่งเล่น:ปิด';
+							}else if($message['text']==="kit 1"){
+								$mixmsg = 'ไฟห้องครัว:เปิด';
+							}else if($message['text']==="kit 0"){
+								$mixmsg = 'ไฟห้องครัว:ปิด';
+							}
+						}
 						$mixmsg = 'คุณกำลังควบคุมด้วยมือ';
 					}else{	$mixmsg = 'What is the mode?';
 					}
